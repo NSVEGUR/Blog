cc0f1d59d5389c300c9ab092a49355bfdbd8973d
NSVEGUR <cs20b1016@iiitdm.ac.in>
2021-07-11 17:04:46 +0530

    added projects

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 'use strict';
 
 const splash = document.querySelector('.splash');
+const home = document.getElementById('#section1');
+const articles = document.getElementById('#section2');
+const works = document.getElementById('#section3');
+const articlesBtn = document.querySelector('.art');
+const worksBtn = document.querySelector('.pro');
+const homeBtn = document.querySelector('.hom');
 
-document.addEventListener('DOMContentLoaded', (e) => {
-  setTimeout(() => {
-    splash.classList.add('display-none');
-  }, 3000);
-});
-let flagMode = 1;
+if (sessionStorage.isVisited) {
+  splash.classList.add('display-none');
+}
+
+if (!sessionStorage.isVisited) {
+  document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
+      splash.classList.add('display-none');
+    }, 3000);
+  });
+  sessionStorage.isVisited = 'true';
+}
 let flagOverlay = 1;
 let dropDown = document.querySelector('.intro');
 let dropOverlay = document.querySelector('.body-overlay');
@@ -20,7 +32,7 @@ const light = function () {
   document.querySelector('.mode').style.backgroundColor = '#000000';
   document.querySelector('.mode').style.color = '#ffffff';
   document.querySelector('.drop').style.color = '#000000';
-  flagMode = flagMode ? 0 : 1;
+  localStorage.setItem('themeCount', '1');
 }
 
 const dark = function () {
@@ -30,14 +42,19 @@ const dark = function () {
   document.querySelector('.mode').style.backgroundColor = '#ffffff';
   document.querySelector('.mode').style.color = '#000000';
   document.querySelector('.drop').style.color = '#ffffff';
-  flagMode = flagMode ? 0 : 1;
+  localStorage.setItem('themeCount', '0');
 }
 
-light();
-
+if (localStorage.isVisited) {
+  Number(localStorage.getItem('themeCount')) ? light() : dark();
+}
+if (!localStorage.isVisited) {
+  light();
+  localStorage.isVisited = 'true';
+}
 document.querySelector('.mode').addEventListener
   ('click', function () {
-    flagMode ? light() : dark();
+    Number(localStorage.getItem('themeCount')) ? dark() : light();
   });
 
 document.querySelector('.drop').addEventListener
@@ -63,3 +80,28 @@ document.querySelector('.body-overlay').addEventListener('click', function () {
   flagOverlay ? document.querySelector('.drop').textContent = '–' : document.querySelector('.drop').textContent = '≡';
   flagOverlay = flagOverlay ? 0 : 1;
 })
+
+
+homeBtn.addEventListener('click',
+  (e) => {
+    e.preventDefault();
+    home.scrollIntoView({
+      behavior: "smooth"
+    })
+  })
+
+articlesBtn.addEventListener('click',
+  (e) => {
+    e.preventDefault();
+    articles.scrollIntoView({
+      behavior: "smooth"
+    })
+  })
+
+worksBtn.addEventListener('click',
+  (e) => {
+    e.preventDefault();
+    works.scrollIntoView({
+      behavior: "smooth"
+    })
+  })
