c7f950a84367101f0e30c7fd412516c686843a60
NSVEGUR <cs20b1016@iiitdm.ac.in>
2021-07-13 21:11:12 +0530

    added themes for articles

--- Articles/common.js
+++ Articles/common.js
@@ -3,6 +3,24 @@
 const nav = document.querySelector('.course-navigation');
 const navBtn = document.querySelector('.art');
 const homeBtn = document.querySelector('.hom');
+const modeBtn = document.querySelector('.mode');
+
+const theme = function (flag) {
+  if (flag) {
+    document.querySelector('body').style.color = '#000000';
+    document.querySelector('body').style.backgroundColor = '#f2f2f2';
+    localStorage.setItem('modeFlag', 0);
+  }
+  else {
+    document.querySelector('body').style.color = '#ffffff';
+    document.querySelector('body').style.backgroundColor = '#121212';
+    localStorage.setItem('modeFlag', 1);
+  }
+}
+
+modeBtn.addEventListener('click', () => {
+  Number(localStorage.getItem('modeFlag')) ? theme(1) : theme(0);
+})
 
 homeBtn.addEventListener('click',
   () => {
